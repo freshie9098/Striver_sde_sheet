487263752c7b8aedc8bc9b8630ec8a29647b5f95
freshie9098 <57678075+freshie9098@users.noreply.github.com>
2022-06-15 18:15:54 +0530

    Update Subset-II.cpp

--- Day 9: Recursion/Subset-II.cpp
+++ Day 9: Recursion/Subset-II.cpp
@@ -2,14 +2,8 @@ class Solution {
     private:
     void f(vector<int>&nums,int i,vector<vector<int>>&ans,vector<int>&a){
        ans.push_back(a);
-        if(i==nums.size()){
-           // ans.push_back(a);
-           return;
-       }
-        // f(nums,i+1,ans,a);
         for(int j = i;j<nums.size();++j){
             if(j>i&&nums[j]==nums[j-1])continue;
-            // f(nums,j+1,ans,a);
             a.push_back(nums[j]);
            
             f(nums,j+1,ans,a);
@@ -23,7 +17,6 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>a;
-        // ans.push_back({});
         f(nums,0,ans,a);
         return ans;
         
