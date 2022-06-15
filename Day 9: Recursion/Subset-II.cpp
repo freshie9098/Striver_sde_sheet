9391f3d819762b701309da07e22ddaf593f043f3
freshie9098 <57678075+freshie9098@users.noreply.github.com>
2022-06-15 18:14:15 +0530

    Update Subset-II.cpp

--- Day 9: Recursion/Subset-II.cpp
+++ Day 9: Recursion/Subset-II.cpp
@@ -1,31 +1,31 @@
-#include<bits/stdc++.h>
-
-void f(vector<int>&nums,int i,map<vector<int>,int>&m,vector<vector<int>>&ans,vector<int>a){
+class Solution {
+    private:
+    void f(vector<int>&nums,int i,vector<vector<int>>&ans,vector<int>&a){
+       ans.push_back(a);
         if(i==nums.size()){
-            if(m.count(a)==0){
-                ans.push_back(a);
-                m[a]++;
-            }
-            return ;
+           // ans.push_back(a);
+           return;
+       }
+        // f(nums,i+1,ans,a);
+        for(int j = i;j<nums.size();++j){
+            if(j>i&&nums[j]==nums[j-1])continue;
+            // f(nums,j+1,ans,a);
+            a.push_back(nums[j]);
+           
+            f(nums,j+1,ans,a);
+            a.pop_back();
         }
-        //npick
-        f(nums,i+1,m,ans,a);
-        //pick
-        a.push_back(nums[i]);
-        f(nums,i+1,m,ans,a);
         
     }
-vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        map<vector<int>,int>m;
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        f(nums,0,m,ans,{});
-    sort(ans.begin(),ans.end());
+        vector<int>a;
+        // ans.push_back({});
+        f(nums,0,ans,a);
         return ans;
         
     }
-vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
-{
-    // Write your code here.
-    return subsetsWithDup(arr);
-}
+};
